2c4444f3a645d7a77dfdfec60d21a8f3a9b04c4d
מאיר סעדון <45739995+MeirSadon@users.noreply.github.com>
2019-06-12 16:57:10 +0300

    Add files via upload

---
+++ hw0206 - ThreadExecutor/hw0206 - ThreadExecutor/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace hw0206___ThreadExecutor
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            ThreadExecutor te = new ThreadExecutor();
+            for (int i = 0; i < 2; i++)
+            {
+                te.AddThread(new Thread(() =>
+                {
+                    for (int j = 1; j <= 10; j++)
+                    {
+                        Console.WriteLine(j);
+                        Thread.Sleep(50);
+                    }
+                }));
+                te.AddThread(new Thread(() => Console.WriteLine("Hello World")));
+            }
+            while (true)
+            {
+
+            }
+        }
+    }
+}
---
+++ hw0206 - ThreadExecutor/hw0206 - ThreadExecutor/ThreadExecutor.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Timers;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+
+namespace hw0206___ThreadExecutor
+{
+    class ThreadExecutor
+    {
+        public Queue<Thread> threads = new Queue<Thread>();
+        public ThreadExecutor()
+        {
+            ////Way 1 - Just Function.
+            Thread exeThread = new Thread(Execute);
+
+            ////Way 2 - With Timer.
+            //System.Timers.Timer t = new System.Timers.Timer(1000);
+            //t.AutoReset = true;
+            //t.Elapsed += EverySecondWithTimer;
+            //t.Start();
+
+            //Way 3 - With ThreadPool.
+            //ThreadPool.QueueUserWorkItem(EverySecondWithThreadPool);
+        }
+
+        public void AddThread(Thread t)
+        {
+                threads.Enqueue(t);
+        }
+        public void Execute()
+        {
+            lock (this)
+            {
+                while (threads.Count > 0)
+                {
+                    Thread t = threads.Dequeue();
+                    t.Start();
+                    t.Join();
+                }
+            }                    
+        }
+        private void EverySecond()
+        {
+            while (true)
+            {
+                while (threads.Count > 0)
+                {
+                    Execute();
+                }
+                Thread.Sleep(1000);
+            }
+        }
+        private void EverySecondWithTimer(object sender, EventArgs e)
+        {
+            while (threads.Count > 0)
+            {
+                Execute();
+            }
+        }
+        private void EverySecondWithThreadPool(object obj)
+        {
+            while (true)
+            {
+                lock (this)
+                {
+                    if (threads.Count > 0)
+                    {
+                        Execute();
+                    }
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
